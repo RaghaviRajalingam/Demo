d7bd814c98c9bc73aedc27312539930fccdf4c66
Raghavi R L <raghavi.rajalingam@aspiresys.com>
2022-07-19 13:58:24 +0530

    API Integration for team list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule , ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -22,6 +23,7 @@ import {MatIconModule} from '@angular/material/icon';
     AppRoutingModule,
     FormsModule,
     ReactiveFormsModule,
+    HttpClientModule,
     BrowserAnimationsModule,
     MatButtonModule,
     MatToolbarModule,
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ServicesService } from '../services.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,10 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
-
-  constructor() { }
+  details:any;
+  constructor(private readonly service:ServicesService) { }
 
   ngOnInit(): void {
+    this.service.getDetails()
+    .subscribe(response => {
+      this.details = response;
+      console.log("API Details",this.details);
+      console.log("Array",this.details.Team);
+      console.log("Details",this.details.Team[0].GameActivityTag);
+    });
   }
-
 }
