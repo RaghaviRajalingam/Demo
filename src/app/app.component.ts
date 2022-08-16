76fda110b0a09cbfff511bd9c07489eea84f7bb2
Raghavi R L <raghavi.rajalingam@aspiresys.com>
2022-08-16 16:43:49 +0530

    Subject's

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,34 +1,65 @@
 import { Component, OnInit } from '@angular/core';
-import { from } from 'rxjs';
+import { AsyncSubject,BehaviorSubject,Observable, from } from 'rxjs';
 import { Router } from '@angular/router';
+//BehaviorSubject from rxjs
+const subject = new BehaviorSubject(123);
+subject.subscribe(console.log);
+subject.subscribe(console.log);
+subject.next(456);
+subject.subscribe(console.log);
+subject.next(789);
+// output: 123, 123, 456, 456, 456, 789, 789, 789
+
+//AsyncSubject from rxjs
+const sub = new AsyncSubject();
+sub.subscribe(console.log);
+sub.next(123); //nothing logged
+sub.subscribe(console.log);
+sub.next(456); //nothing logged
+sub.complete(); //456, 456 logged by both subscribers
+
 const array = [1, 2, 'three', true];
 
 from(array)
-	.subscribe((val: any) => console.log(val));
+  .subscribe((val: any) => console.log(val));
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  
-constructor(private readonly router: Router) { }
+
+export class AppComponent implements OnInit  {
+  value: any;
+
+  constructor(private readonly router: Router) { }
   ngOnInit() {
+
+    //map using rxjs
+    // const observable = of(1, 2, 3, 4, 5, 6);
+    // const subscription = observable.pipe(
+    //   map(val => val * val)
+    // ).subscribe(val => console.log(val));
+
+    //Filter the value using rxjs
+    // const observable = of(1, 2, 3, 4, 5, 6);
+    // const subscription = observable.pipe(
+    //   filter(val => val % 2 === 0)
+    // ).subscribe(val => console.log(val));
   }
-gotoForm() {
-  this.router.navigate(['/register']);
-}
-gotoApiDetails() {
-  this.router.navigate(['/apidetails']);
-}
-gotoChart() {
-  this.router.navigate(['/chart']);
-}
-gotocontent() {
-  this.router.navigate(['/ngContent']);
-}
-gotoNgrxStore() {
-  this.router.navigate(['/ngrxstore']);
-}
+  gotoForm() {
+    this.router.navigate(['/register']);
+  }
+  gotoApiDetails() {
+    this.router.navigate(['/apidetails']);
+  }
+  gotoChart() {
+    this.router.navigate(['/chart']);
+  }
+  gotocontent() {
+    this.router.navigate(['/ngContent']);
+  }
+  gotoNgrxStore() {
+    this.router.navigate(['/ngrxstore']);
+  }
 }
 
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,13 +1,35 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Observable } from 'rxjs';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent implements OnInit {
+export class DashboardComponent implements OnInit, OnDestroy {
+  value: any;
   constructor() { }
 
   ngOnInit(): void {
+    const observable = new Observable(subscriber => {
+      subscriber.next(1);
+      subscriber.next(2);
+      subscriber.next(3);
+      setTimeout(() => {
+        subscriber.next(4);
+        subscriber.complete();
+      }, 1000);
+    });
+
+    console.log('just before subscribe');
+    this.value = observable.subscribe({
+      next(x) { console.log('got value ' + x); },
+      error(err) { console.error('something wrong occurred: ' + err); },
+      complete() { console.log('done'); }
+    });
+    console.log('just after subscribe');
+    console.log("Value",this.value);
+  }
+  ngOnDestroy() {
+    this.value.unsubscribe();
   }
 }
